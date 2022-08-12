fc9536e2a1dba5b0387b45f709ff3135b2412bd6
David Yates <yates.programmer@gmail.com>
2022-08-11 18:46:08 -0700

    More changes.

--- Normal/DotNet6CoreIsolated/DotNetCoreIsolated/Program.cs
+++ Normal/DotNet6CoreIsolated/DotNetCoreIsolated/Program.cs
@@ -1,13 +1,16 @@
 using DotNetCoreIsolated;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults(workerApplication =>
+    .ConfigureFunctionsWorkerDefaults(builder =>
     {
         // https://docs.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide#middleware
         // Register our custom middleware with the worker
-        workerApplication.UseMiddleware<ExceptionMiddleware>();
+        builder.UseMiddleware<ExceptionMiddleware>();
 
+        // Application Insights open issue
+        // https://github.com/Azure/azure-functions-dotnet-worker/issues/760
 
         // Application Insights not supported in a Nuget package yet! It's mentioned as unsupported here at the bottom of this table:
         // https://docs.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide#differences-with-net-class-library-functions
@@ -18,9 +21,8 @@ var host = new HostBuilder()
 
         // This may be the solution for Application Insights: https://docs.microsoft.com/en-us/azure/azure-monitor/app/worker-service#using-application-insights-sdk-for-worker-services
         // https://www.nuget.org/packages/Microsoft.ApplicationInsights.WorkerService
-
-
-        //workerApplication.UseWhen<StampHttpHeaderMiddleware>((context) =>
+        
+        //builder.UseWhen<StampHttpHeaderMiddleware>((context) =>
         //{
         //    // We want to use this middleware only for http trigger invocations.
         //    return context.FunctionDefinition.InputBindings.Values
@@ -29,8 +31,17 @@ var host = new HostBuilder()
     })
     .ConfigureServices(s =>
     {
-        s.AddDotNetCoreIsolatedDependencies();
+        s.AddLogging();
+        
+        // Requires Microsoft.ApplicationInsights.WorkerService NuGet package.
+        // Despite what you read here, a connection string uses APPLICATIONINSIGHTS_CONNECTION_STRING within the values object.
+        // You can see this if you create a new Azure Function App in the portal and specify you want Application Insights.
+        // https://docs.microsoft.com/en-us/azure/azure-monitor/app/worker-service#net-core-lts-worker-service-application
+        //  s.AddApplicationInsightsTelemetryWorkerService("92c3da69-756b-46f7-9ad9-2c3ac4ca0908");
+        s.AddApplicationInsightsTelemetryWorkerService();
 
+
+        s.AddDotNetCoreIsolatedDependencies();
     })
     .Build();
 
