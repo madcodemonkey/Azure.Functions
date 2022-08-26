8d4b0c27e5681155c1713c9ee6ac759b5dbc45ad
David Yates <yates.programmer@gmail.com>
2022-08-25 17:59:30 -0700

    Comments updated.

--- ServiceBus/IsolatedQueueExample/IsolatedQueueExample/Program.cs
+++ ServiceBus/IsolatedQueueExample/IsolatedQueueExample/Program.cs
@@ -3,19 +3,21 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using QueueExample;
 
-// See Connect to App Configuration: https://docs.microsoft.com/en-us/azure/azure-app-configuration/quickstart-azure-functions-csharp?tabs=isolated-process#connect-to-an-app-configuration-store
 
 var host = new HostBuilder()
     .ConfigureAppConfiguration(builder =>
     {
-        var azureAppConfigurationEndpointUri = Environment.GetEnvironmentVariable("AzureAppConfigurationEndpoint");
-
         // You might need this depending on your local dev env
         // var credentials = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ExcludeSharedTokenCacheCredential = true });
-        // Requires Azure.Identity Nuget package.
         var credentials = new DefaultAzureCredential();
 
-        // Requires the Microsoft.Extensions.Configuration.AzureAppConfiguration
+        // Required NuGet packages for Azure App Configuration:
+        // 1. Azure.Identity
+        // 2. Microsoft.Extensions.Configuration.AzureAppConfiguration
+
+        var azureAppConfigurationEndpointUri = Environment.GetEnvironmentVariable("AzureAppConfigurationEndpoint");
+
+        // Docs: https://docs.microsoft.com/en-us/azure/azure-app-configuration/quickstart-azure-functions-csharp?tabs=isolated-process#connect-to-an-app-configuration-store
         builder.AddAzureAppConfiguration(options =>
         {
             options.Connect(new Uri(azureAppConfigurationEndpointUri), credentials)
@@ -24,6 +26,22 @@ var host = new HostBuilder()
                     kv.SetCredential(credentials);
                 });
         });
+
+        // Required NuGet packages for direct key vault connection:
+        // 1. Azure.Identity
+        // 2. Azure.Extensions.AspNetCore.Configuration.Secrets 
+
+        // Example: https://github.com/mizrael/AzureFunction-KeyVault/blob/main/Program.cs
+        //builder.AddEnvironmentVariables();
+
+         //           var tmpConfig = builder.Build();
+         //           var vaultUri = tmpConfig["AzureKeyVaultEndpoint"];
+         //           if(string.IsNullOrWhiteSpace(vaultUri)){
+         //               throw new ArgumentException("please provide a valid VaultUri");
+         //           }
+
+         //           builder.AddAzureKeyVault(new Uri(vaultUri), credentials);
+
     })
     .ConfigureFunctionsWorkerDefaults(builder =>
     {
